28f0a3527bdeaa260edee589bf8d75949e2f350f
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-06-09 10:26:31 +0100

    Create _1_FillTheMatrix.java

---
+++ _4_Multidimentional Arrays Exercise/_1_FillTheMatrix.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class _1_FillTheMatrix {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      //"3, A"
+        String [] input = scanner.nextLine().split(", ");
+        int n = Integer.parseInt(input[0]);
+        String pattern = input[1];
+
+        int [][] matrix = new int[n][n];
+        //1.Проверяваме шаблона:
+        switch (pattern){
+            //2.Извикваме правилния метод
+            case "A" -> fillMatrixPatternA(matrix);
+            case "B" -> fillMatrixPatternB(matrix);
+        }
+        //3.Отпечатваме матрицата
+        printMatrix(matrix);
+    }
+
+    private static void fillMatrixPatternB(int[][] matrix) {
+        int counter = 1;
+        for (int col = 0; col < matrix.length; col++) {
+            //ЗА ВСЯКА ЕДНА КОЛОНА ОБХОЖДАМЕ РЕДОВЕТЕ
+
+            if (col % 2 == 0){ //AKO колоната е четно число: 0 ред до последен
+                for (int row = 0; row < matrix.length; row++) {
+                    matrix[row][col] = counter++;
+                }
+
+            }//ако колоната е нечетно число: последен до 0 ред
+            else {
+                for (int row = matrix.length -1; row >= 0; row--) {
+                    matrix[row][col] = counter++;
+                }
+            }
+        }
+    }
+
+    private static void fillMatrixPatternA(int[][] matrix) {
+        int counter = 1;
+        //ЗА ВСЯКА ЕДНА КОЛОНА ОБХОЖДАМЕ РЕДОВЕТЕ
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = 0; row < matrix.length; row++) {
+                matrix[row][col] = counter++;
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

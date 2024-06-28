6d14454a5494ca5f0bd9a4b2047a814777d1446d
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-06-28 12:31:26 +0100

    Create Card.java

---
+++ Java OOP/_2_Working with abstractiona Lab/_3_CardsWithValue/Card.java
@@ -0,0 +1,15 @@
+package _3_CardsWithValue;
+
+public class Card {
+
+    // силата на картата = силата на боята + силата на ранка
+    private int power;
+
+    public Card(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return this.power;
+    }
+}

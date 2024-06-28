45d53040e8aa3dfb9e0e3d39e39705bc64275e40
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-06-28 12:29:15 +0100

    Create CardRank.java

---
+++ Java OOP/_2_Working with abstractiona Lab/_2_CardRank.java/CardRank.java
@@ -0,0 +1,17 @@
+package _2_CardRank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}

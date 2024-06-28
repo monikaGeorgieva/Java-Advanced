b41c8f4d0f3268173d546aef7cef7bee056ee411
Monika Georgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2024-06-28 12:30:06 +0100

    Create Main.java

---
+++ Java OOP/_2_Working with abstractiona Lab/_3_CardsWithValue/Main.java
@@ -0,0 +1,19 @@
+package _3_CardsWithValue;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //TWO -> card rank
+        CardRank rank = CardRank.valueOf(scanner.nextLine());
+        //CLUBS -> card suit
+        CardSuit suit = CardSuit.valueOf(scanner.nextLine());
+
+        int cardPower = rank.getPower() + suit.getPower();
+
+        Card card = new Card(cardPower);
+
+        System.out.printf("Card name: %s of %s; Card power: %d", rank, suit, card.getPower());
+    }
+}
